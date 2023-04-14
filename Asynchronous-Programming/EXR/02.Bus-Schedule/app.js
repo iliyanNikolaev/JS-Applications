a9d5734048a72e50c87fd4674198875ec885029f
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-04-14 10:02:57 +0300

    init

--- Asynchronous-Programming/EXR/02.Bus-Schedule/app.js
+++ Asynchronous-Programming/EXR/02.Bus-Schedule/app.js
@@ -1,11 +1,39 @@
 function solve() {
+    
+    let stop = {
+        next: 'depot'
+    }
+    
+
+    let container = document.getElementById('info');
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
 
     function depart() {
-        console.log('Depart TODO...');
+        let url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+
+        let response = fetch(url)
+            .then(response => response.json())
+            .then(response => {
+
+                stop = response;
+
+                container.textContent = `Next stop ${stop.name}`;
+
+                departBtn.disabled = true;
+                arriveBtn.disabled = false;
+            })
+            .catch(err => {
+                container.textContent = 'Error';
+                departBtn.disabled = true;
+                arriveBtn.disabled = true;
+            });
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        container.textContent = `Arriving at ${stop.name}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
