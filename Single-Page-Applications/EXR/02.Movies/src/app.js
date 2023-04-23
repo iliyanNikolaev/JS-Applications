3207f0192def4f57f29ca55aa3546622ff92d48a
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-04-23 11:04:19 +0300

    add main routing logic

--- Single-Page-Applications/EXR/02.Movies/src/app.js
+++ Single-Page-Applications/EXR/02.Movies/src/app.js
@@ -1,1 +1,56 @@
-console.log('it works')
+const homePage = document.getElementById('home-page');
+const loginPage = document.getElementById('login-page');
+const registerPage = document.getElementById('register-page');
+
+viewHomePage();
+
+document.getElementById('navigation').addEventListener('click', onNavigate);
+
+function onNavigate(e){
+    if(e.target.tagName === 'A'){
+        e.preventDefault();
+
+        if(e.target.href){
+            const url = new URL(e.target.href);
+
+            const path = url.pathname;
+
+            const renderer = routes[path];
+
+            renderer();
+        }
+    }
+}
+
+const routes = {
+    '/home': viewHomePage,
+    '/login': viewLoginPage,
+    '/register': viewRegisterPage,
+    '/logout': onLogout,
+}
+
+function  hideAll(){
+    const views = document.querySelectorAll('.view-section');
+
+    views.forEach(v => v.style.display = 'none');
+}
+
+function viewHomePage(){
+    hideAll();
+    homePage.style.display = 'block';
+}
+
+function viewLoginPage(){
+    hideAll();
+    loginPage.style.display = 'block';
+}
+
+function viewRegisterPage(){
+    hideAll();
+    registerPage.style.display = 'block';
+}
+
+function onLogout(){
+    viewHomePage();
+    alert('successful logout')
+}
