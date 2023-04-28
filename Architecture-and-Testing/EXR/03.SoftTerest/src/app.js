a90692421e5ac3fc2b294951c65aceb379049663
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-04-28 19:55:47 +0300

    dependency injection
    -dependency injection from app.js to home.js
    -add some basic views logic
    -some html changes

--- Architecture-and-Testing/EXR/03.SoftTerest/src/app.js
+++ Architecture-and-Testing/EXR/03.SoftTerest/src/app.js
@@ -1,2 +1,33 @@
-import * as users from './api/users.js';
-window.users = users;
+import { showHome } from "./views/home.js";
+
+const main = document.querySelector('main');
+const registerPage = document.querySelector('#registerPage');
+const loginPage = document.querySelector('#loginPage');
+const detailsPage = document.querySelector('#detailsPage');
+const catalogPage = document.querySelector('#dashboard-holder');
+const createPage = document.querySelector('#createPage');
+const views = document.querySelector('#views');
+
+views.remove();
+
+const links = {
+    '/': showHome,
+    '/catalog': catalogPage,
+    '/login': loginPage,
+    '/details': detailsPage,
+    '/create': createPage,
+    '/register': registerPage
+}
+
+function showPage(section){
+    main.replaceChildren(section);
+}
+
+const context = {
+    showPage
+}
+
+window.showHome = () => {
+    showHome(context);
+}
+
