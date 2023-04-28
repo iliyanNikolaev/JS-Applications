2a156a01a9db3e3d573faac041d91cbfa47d5e78
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-04-28 21:58:07 +0300

    add login functionality

--- Architecture-and-Testing/EXR/03.SoftTerest/src/views/login.js
+++ Architecture-and-Testing/EXR/03.SoftTerest/src/views/login.js
@@ -1,5 +1,24 @@
+import { login } from "../api/users.js";
+
 const section = document.querySelector('#loginPage');
+const form = section.querySelector('form');
+form.addEventListener('submit', onLogin);
+
+let ctx = null;
 
 export function showLogin(context){
+    ctx = context;
     context.showPage(section);
+}
+
+async function onLogin(e){
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    await login(email, password);
+    ctx.goto('/');
 }
