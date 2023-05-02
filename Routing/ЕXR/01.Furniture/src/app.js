7b7e88f409c07470e3656eaf2d11d5af97184456
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-05-02 20:53:00 +0300

    create routing table with 'console.log arrows'

--- Routing/ЕXR/01.Furniture/src/app.js
+++ Routing/ЕXR/01.Furniture/src/app.js
@@ -1,1 +1,10 @@
+page('/', () => console.log('home view'));
+page('/details/:id', () => console.log('details view'));
+page('/create', () => console.log('create view'));
+page('/edit/:id', () => console.log('edit view'));
+page('/details', () => console.log('details view'));
+page('/login', () => console.log('login view'));
+page('/register', () => console.log('register view'));
+page('/my-furniture', () => console.log('my furniture view'));
 
+page.start();
