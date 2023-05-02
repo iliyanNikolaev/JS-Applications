526508cf07c1a156de3b973b416f124bd54a2ba6
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-05-02 21:37:20 +0300

    add auth.js module
    login, register, logout functionality

---
+++ Routing/ЕXR/01.Furniture/src/api/auth.js
@@ -0,0 +1,26 @@
+import { clearUserData, setUserData } from '../util.js';
+import { get, post } from './api.js';
+
+const endpoints = {
+    login: '/users/login',
+    register: '/users/register',
+    logout: '/users/logout'
+}
+
+export async function login(email, password) {
+    const user = await post(endpoints.login, {email, password});
+
+    setUserData(user);
+}
+
+export async function register(email, password) {
+    const user = await post(endpoints.register, {email, password});
+
+    setUserData(user);
+}
+
+export async function logout() {
+    get(endpoints.logout);
+    
+    clearUserData();
+}
