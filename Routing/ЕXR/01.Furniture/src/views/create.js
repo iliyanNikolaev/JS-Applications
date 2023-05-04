86e3788c1aec98f327b2e3d397963e286f3e2749
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-05-04 19:40:45 +0300

    add create.js view and functionality

--- Routing/ЕXR/01.Furniture/src/views/create.js
+++ Routing/ЕXR/01.Furniture/src/views/create.js
@@ -1,5 +1,72 @@
+import { createItem } from "../api/data.js";
 import { html } from "../lib.js";
 
-export function createPage(){
-    console.log('create view');
+const createTemplate = (onCreate, error) => html`
+<div class="row space-top">
+            <div class="col-md-12">
+                <h1>Create New Furniture</h1>
+                <p>Please fill all fields.</p>
+                ${error ? html`<p class="error-field">${error}</p>` : null}
+            </div>
+        </div>
+        <form @submit=${onCreate}>
+            <div class="row space-top">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="form-control-label" for="new-make">Make</label>
+                        <input class="form-control valid" id="new-make" type="text" name="make">
+                    </div>
+                    <div class="form-group has-success">
+                        <label class="form-control-label" for="new-model">Model</label>
+                        <input class="form-control" id="new-model" type="text" name="model">
+                    </div>
+                    <div class="form-group has-danger">
+                        <label class="form-control-label" for="new-year">Year</label>
+                        <input class="form-control" id="new-year" type="number" name="year">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="new-description">Description</label>
+                        <input class="form-control" id="new-description" type="text" name="description">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="form-control-label" for="new-price">Price</label>
+                        <input class="form-control" id="new-price" type="number" name="price">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="new-image">Image</label>
+                        <input class="form-control" id="new-image" type="text" name="img">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="new-material">Material (optional)</label>
+                        <input class="form-control" id="new-material" type="text" name="material">
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Create" />
+                </div>
+            </div>
+        </form>`
+
+export function createPage(ctx){
+    ctx.render(createTemplate(onCreate));
+
+    async function onCreate(e){
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData.entries());
+
+        try {
+            for (const entry in data) {
+                if(entry != 'material' && data[entry] == ''){
+                    throw new Error('Please fill all non-optional fields.');
+                }
+            }
+
+            await createItem(data);
+            ctx.page.redirect('/');
+        } catch (err) {
+            ctx.render(createTemplate(onCreate, err.message));
+        }
+    }
 }
--- Routing/ЕXR/01.Furniture/src/views/details.js
+++ Routing/ЕXR/01.Furniture/src/views/details.js
@@ -1,5 +1,7 @@
 import { html } from "../lib.js";
 
+const detailsTemplate = () => html``;
+
 export function detailsPage(ctx){
     const id = ctx.params.id;
     console.log(`details view ${id}`);
