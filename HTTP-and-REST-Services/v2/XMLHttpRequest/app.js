8f4f0f7282dc4a94db36e1f86a36d0319d4148dd
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-10-16 10:02:22 +0300

    XMLHttpRequest example

---
+++ HTTP-and-REST-Services/v2/XMLHttpRequest/app.js
@@ -0,0 +1,17 @@
+import { appKey, jsKey } from "../constants.js";
+
+document.querySelector('button').addEventListener('click', loadLaptops);
+
+function loadLaptops() {
+    const url = 'https://parseapi.back4app.com/classes/Laptops';
+    const req = new XMLHttpRequest();
+
+    req.addEventListener('readystatechange', () => {
+        document.querySelector('#response').textContent = req.responseText;
+    });
+
+    req.open('GET', url);
+    req.setRequestHeader('X-Parse-Application-Id', appKey);
+    req.setRequestHeader('X-Parse-Javascript-Key', jsKey);
+    req.send();
+}
